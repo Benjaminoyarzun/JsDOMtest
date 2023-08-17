d4a0d47139f5488ed0390c2cadc3fa3d54280412
Benjaminoyarzun <104220399+Benjaminoyarzun@users.noreply.github.com>
2023-08-17 10:25:58 -0300

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,6 @@
+function crearEnlaces() {
+    //Utilizando el innerHTML
+    let ob1 = document.getElementById("enlaces1")
+    ob1.innerHTML = `<a href="${document.getElementById('text1').value}">Enlace 1</a>
+                     <br><a href="${document.getElementById('text2').value}">Enlace 2</a>`
+}
